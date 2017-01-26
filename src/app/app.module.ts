c0fb67d20f78c6e3b2996bfb4b0a3e9cd933a882
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2017-01-26 15:50:18 +0100

    (bug) compile error because of outdated reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpModule } from '@angular/http';
 import { NgReduxModule, DevToolsExtension } from 'ng2-redux';
 import { NgReduxRouter } from 'ng2-redux-router';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './components/about-page.component';
 import { QuotesPageComponent } from './components/quotes-page.component';
@@ -26,8 +24,7 @@ import { AppRoutesModule } from './app.routes';
     BrowserModule,
     HttpModule,
     NgReduxModule,
-    AppRoutesModule,
-    NgbModule.forRoot()
+    AppRoutesModule
   ],
   providers: [
     DevToolsExtension,
