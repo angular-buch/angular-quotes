478ab239542ed8da270a4f89173dcacd3fdbe551
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-05-18 21:44:43 +0200

    fix: combineReducers is not of type IAppState
    @fmalcher01

--- src/app/reducers/rootReducer.ts
+++ src/app/reducers/rootReducer.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux-seamless-immutable';
 import { routerReducer } from '@angular-redux/router';
 import { quotesReducer } from './quotes.reducer';
-import { IAppState } from './types';
 
-// rootReducer of IAppState
 export const rootReducer = combineReducers({
   quotes: quotesReducer,
   router: routerReducer
